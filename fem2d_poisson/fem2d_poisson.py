60cf4a1bfc439708a68e44cea6d89e77aa6ed7ef
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-05-26 13:14:27 +0900

    Update fem2d_poisson.py

--- fem2d_poisson/fem2d_poisson.py
+++ fem2d_poisson/fem2d_poisson.py
@@ -14,7 +14,7 @@ from matplotlib import cm  #カラーマップ
 
 #節点データを生成
 def generate_nodes(node_type):
-    #格子状の配置
+    #格子点配置
     if (node_type[0]=='lattice'):
         lattice_num = node_type[1]  #格子分割におけるx・y方向の節点数
         x = np.linspace(x_min, x_max, lattice_num)
@@ -26,46 +26,47 @@ def generate_nodes(node_type):
                 nodes[i +lattice_num*j, 0] = x[i]
                 nodes[i +lattice_num*j, 1] = y[j]
 
-    #ランダムな配置
+    #ランダム配置
     elif (node_type[0]=='random'):
         random_num = node_type[1]  #ランダム分割における節点数
         nodes = np.random.rand(random_num,2)  #[0~1,0~1]の点をrandom_num個生成
         nodes[:,0] = x_min +(x_max-x_min)*nodes[:,0]
         nodes[:,1] = y_min +(y_max-y_min)*nodes[:,1]
 
-        #四隅に点を移動
-        if (8<=random_num):
-            nodes[0,0],nodes[0,1] = (x_min, y_min)
-            nodes[1,0],nodes[1,1] = (x_min, y_max)
-            nodes[2,0],nodes[2,1] = (x_max, y_min)
-            nodes[3,0],nodes[3,1] = (x_max, y_max)
-            nodes[4,0],nodes[4,1] = (x_min, (y_max+y_min)/2)
-            nodes[5,0],nodes[5,1] = (x_max, (y_max+y_min)/2)
-            nodes[6,0],nodes[6,1] = ((x_max+x_min)/2, y_min)
-            nodes[7,0],nodes[7,1] = ((x_max+x_min)/2, y_max)  #'''
+        #隅に点を移動
+        if (4<=random_num):
+            nodes[0,0],nodes[0,1] = (x_min, (y_max+y_min)/2)
+            nodes[1,0],nodes[1,1] = (x_max, (y_max+y_min)/2)
+            nodes[2,0],nodes[2,1] = ((x_max+x_min)/2, y_min)
+            nodes[3,0],nodes[3,1] = ((x_max+x_min)/2, y_max)
+            '''nodes[4,0],nodes[4,1] = (x_min, y_min)
+            nodes[5,0],nodes[5,1] = (x_min, y_max)
+            nodes[6,0],nodes[6,1] = (x_max, y_min)
+            nodes[7,0],nodes[7,1] = (x_max, y_max)  #'''
 
-    tri = scipy.spatial.Delaunay(nodes)  #節点をドロネー分割
+    #節点をドロネー分割
+    delaunay_data = scipy.spatial.Delaunay(nodes)
 
-    nod_total = tri.points.shape[0]
-    tri_ele_total = tri.simplices.shape[0]
-    seg_ele_total = tri.convex_hull.shape[0]
+    nod_total = delaunay_data.points.shape[0]
+    tri_ele_total = delaunay_data.simplices.shape[0]
+    seg_ele_total = delaunay_data.convex_hull.shape[0]
     print('節点数、三角形要素数、境界線分要素数')
     print(nod_total, tri_ele_total, seg_ele_total)
 
-    nod_pos_glo = tri.points  #[nod_total,2]
-    print('Global節点の座標\n', nod_pos_glo)
+    nod_pos_glo = delaunay_data.points  #[nod_total,2]
+    print('Global節点のx,y座標\n', nod_pos_glo)
 
-    nod_num_tri = tri.simplices  #[tri_ele_total,3]
-    print('三角形要素の節点番号\n', nod_num_tri)
+    nod_num_tri = delaunay_data.simplices  #[tri_ele_total,3]
+    print('三角形要素を構成する節点番号\n', nod_num_tri)
 
-    nod_num_seg = tri.convex_hull  #[seg_ele_total,2]
-    print('境界線分要素の節点番号\n', nod_num_seg)
+    nod_num_seg = delaunay_data.convex_hull  #[seg_ele_total,2]
+    print('境界線分要素を構成する節点番号\n', nod_num_seg)
 
     return nod_pos_glo, nod_num_tri, nod_num_seg
 
 
 def make_mesh_data():
-    print('三角形要素のLocal節点座標')
+    print('三角形要素を構成するLocal節点座標')
     nod_pos_tri = np.empty((len(nod_num_tri),3,2), np.float64)  #各要素のLocal節点のx,y座標
     for e in range(len(nod_num_tri)):
         for n in range(3):
@@ -73,7 +74,7 @@ def make_mesh_data():
             nod_pos_tri[e,n,1] = nod_pos_glo[nod_num_tri[e,n], 1]
     print('nod_pos_tri(x0, y0),(x1, y1),(x2, y2) =\n', nod_pos_tri)
 
-    print('境界線分要素のLocal節点座標')
+    print('境界線分要素を構成するLocal節点座標')
     nod_pos_seg = np.empty((len(nod_num_seg),2,2), np.float64)  #各境界要素のLocal節点のx,y座標
     for e in range(len(nod_num_seg)):
         for n in range(2):
@@ -133,7 +134,7 @@ def assemble_global_matrix(matrix_type):
             for j in range(3):
                 mat_A_glo[ nod_num_tri[e,i], nod_num_tri[e,j] ] \
                     += 1.0/(4*area_tri[e]) *(shape_b[e,i]*shape_b[e,j] +shape_c[e,i]*shape_c[e,j])
-            vec_b_glo[ nod_num_tri[e,i] ] += func_f *area_tri[e]/3.0
+            vec_b_glo[ nod_num_tri[e,i] ] += -func_f *area_tri[e]/3.0
 
         #処理の経過を％表示
         if(CountPercent <= 100*e/len(nod_pos_tri)):
@@ -165,25 +166,25 @@ def make_boundary_info(nod_pos_seg):
            nod_pos_seg[e,1,0] <= x_min+0.1*leng_seg[e]):  #左側境界
             BC_type[e] = BC_left
             #BC_type[e,:] = BC_left[:]
-        elif(x_max-0.01*leng_seg[e] <= nod_pos_seg[e,0,0] and
-             x_max-0.01*leng_seg[e] <= nod_pos_seg[e,1,0]):  #右側境界
+        elif(x_max-0.1*leng_seg[e] <= nod_pos_seg[e,0,0] and
+             x_max-0.1*leng_seg[e] <= nod_pos_seg[e,1,0]):  #右側境界
             BC_type[e] = BC_right
             #BC_type[e,:] = BC_right[:]
-        elif(nod_pos_seg[e,0,1] <= y_min+0.01*leng_seg[e] and
-             nod_pos_seg[e,1,1] <= y_min+0.01*leng_seg[e]):  #下側境界
+        elif(nod_pos_seg[e,0,1] <= y_min+0.1*leng_seg[e] and
+             nod_pos_seg[e,1,1] <= y_min+0.1*leng_seg[e]):  #下側境界
             BC_type[e] = BC_bottom
             #BC_type[e,:] = BC_bottom[:]
-        elif(y_max-0.01*leng_seg[e] <= nod_pos_seg[e,0,1] and
-             y_max-0.01*leng_seg[e] <= nod_pos_seg[e,1,1]):  #上側境界
+        elif(y_max-0.1*leng_seg[e] <= nod_pos_seg[e,0,1] and
+             y_max-0.1*leng_seg[e] <= nod_pos_seg[e,1,1]):  #上側境界
             BC_type[e] = BC_top
             #BC_type[e,:] = BC_top[:]
     print('BC_type =\n', BC_type)
 
-    return leng_seg, BC_type
+    return BC_type, leng_seg
 
 
 #境界条件を実装
-def set_boundary_condition(mat_A_glo, vec_b_glo, BC_type):
+def set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, leng_seg):
     #各要素の各節点に対応したGlobal節点に対して処理する
     print('Boundary conditions')
     CountPercent = 1
@@ -237,40 +238,40 @@ def solve_simultaneous_equations(mat_A_glo, vec_b_glo):
 
 
 #メッシュを表示
-def visualize_mesh(show_mesh_text, mesh_out_type):
+def visualize_mesh(show_text, out_type):
     #plt.rcParams['font.family'] = 'Times New Roman' #全体のフォントを設定
-    #fig = plt.figure(figsize=(8, 6), dpi=100, facecolor='#ffffff')  #図の設定
+    fig = plt.figure(figsize=(8, 6), dpi=100, facecolor='#ffffff')  #図の設定
 
     #plt.title("2D mesh for FEM") #グラフタイトル
     plt.xlabel('$x$') #x軸の名前
     plt.ylabel('$y$') #y軸の名前
 
     #メッシュをプロット
-    plt.triplot(nod_pos_glo[:,0],nod_pos_glo[:,1], nod_num_tri)  #三角形要素
-    plt.plot(nod_pos_glo[:,0],nod_pos_glo[:,1], 'o')  #節点
+    plt.triplot(nod_pos_glo[:,0],nod_pos_glo[:,1], nod_num_tri, color='#0000ff')  #三角形要素
+    plt.scatter(nod_pos_glo[:,0],nod_pos_glo[:,1], color='#0000ff')  #節点
 
-    if(show_mesh_text==True):
+    if(show_text==True):
         for n in range(len(nod_pos_glo)):  #節点番号
             plt.text(nod_pos_glo[n,0], nod_pos_glo[n,1], n, ha='right')
         for e in range(len(nod_pos_tri)):  #三角形要素番号
-            meanX = (nod_pos_tri[e,0,0] +nod_pos_tri[e,1,0] +nod_pos_tri[e,2,0])/3
-            meanY = (nod_pos_tri[e,0,1] +nod_pos_tri[e,1,1] +nod_pos_tri[e,2,1])/3
+            meanX = (nod_pos_tri[e,0,0] +nod_pos_tri[e,1,0] +nod_pos_tri[e,2,0])/3.0
+            meanY = (nod_pos_tri[e,0,1] +nod_pos_tri[e,1,1] +nod_pos_tri[e,2,1])/3.0
             plt.text(meanX, meanY, '#%d' %e, ha='center')
-        for e in range(len(nod_pos_seg)):  #三角形要素番号
-            meanX = (nod_pos_seg[e,0,0] +nod_pos_seg[e,1,0])/2
-            meanY = (nod_pos_seg[e,0,1] +nod_pos_seg[e,1,1])/2
+        for e in range(len(nod_pos_seg)):  #線分要素番号
+            meanX = (nod_pos_seg[e,0,0] +nod_pos_seg[e,1,0])/2.0
+            meanY = (nod_pos_seg[e,0,1] +nod_pos_seg[e,1,1])/2.0
             plt.text(meanX, meanY, '*%d' %e, ha='center')
 
     #グラフを表示
-    if(mesh_out_type=='show'):
+    if(out_type=='show'):
         plt.show()
-    elif(mesh_out_type=='save'):
+    elif(out_type=='save'):
         plt.savefig("fem2d_mesh.png")
     plt.close()  #作成した図のウィンドウを消す
 
 
 #計算結果を表示
-def visualize_result(show_result_text, result_out_type):
+def visualize_result(nod_pos_glo, unknown_vec_u, show_text, out_type):
     #plt.rcParams['font.family'] = 'Times New Roman'  #全体のフォントを設定
     fig = plt.figure(figsize=(8, 6), dpi=100, facecolor='#ffffff')  #図の設定
     ax = fig.gca(projection='3d', azim=-120, elev=20)  #3Dグラフを設定
@@ -280,26 +281,26 @@ def visualize_result(show_result_text, result_out_type):
     ax.set_ylabel('$y$')  #y軸の名前
     ax.set_zlabel('$u(x,y)$')  #z軸の名前
 
-    #数値計算解をプロット
+    #数値解をプロット
     surf = ax.plot_trisurf(nod_pos_glo[:,0],nod_pos_glo[:,1],unknown_vec_u, cmap=cm.jet, linewidth=0)
     plt.colorbar(surf, shrink=0.8, aspect=10)  #カラーバー
 
-    if(show_result_text==True):
-        #節点番号
-        for n in range(len(nod_pos_glo)):
-            ax.text(nod_pos_glo[n,0],nod_pos_glo[n,1],unknown_vec_u[n], 'n%d' %n, ha='center',va='bottom', color='#000000')
+    #更に体裁を整える
+    plt.legend(loc='best')  #凡例(グラフラベル)を表示
 
-        #三角形要素番号
-        for e in range(len(nod_pos_tri)):
-            meanX = (nod_pos_tri[e,0,0] +nod_pos_tri[e,1,0] +nod_pos_tri[e,2,0])/3
-            meanY = (nod_pos_tri[e,0,1] +nod_pos_tri[e,1,1] +nod_pos_tri[e,2,1])/3
-            meanU = (unknown_vec_u[nod_num_tri[e,0]] +unknown_vec_u[nod_num_tri[e,1]] +unknown_vec_u[nod_num_tri[e,2]])/3
+    if(show_text==True):
+        for n in range(len(nod_pos_glo)):  #節点番号
+            ax.text(nod_pos_glo[n,0],nod_pos_glo[n,1],unknown_vec_u[n], 'n%d' %n, ha='center',va='bottom', color='#000000')
+        for e in range(len(nod_pos_tri)):  #三角形要素番号
+            meanX = (nod_pos_tri[e,0,0] +nod_pos_tri[e,1,0] +nod_pos_tri[e,2,0])/3.0
+            meanY = (nod_pos_tri[e,0,1] +nod_pos_tri[e,1,1] +nod_pos_tri[e,2,1])/3.0
+            meanU = (unknown_vec_u[nod_num_tri[e,0]] +unknown_vec_u[nod_num_tri[e,1]] +unknown_vec_u[nod_num_tri[e,2]])/3.0
             ax.text(meanX, meanY, meanU, 'e%d' %e, ha='center', color='#000000')
 
     #グラフを表示
-    if(result_out_type=='show'):
+    if(out_type=='show'):
         plt.show()
-    elif(result_out_type=='save'):
+    elif(out_type=='save'):
         plt.savefig("fem2d_poisson.png")
     plt.close()  #作成した図のウィンドウを消す
 
@@ -316,12 +317,12 @@ if __name__ == '__main__':
     #左部(x=x_min)、右部(x=x_max)、下部(y=y_min)、上部(y=y_max)の、境界の種類と値
     #境界の種類はDirichlet,Neumann
     BC_left = ['Dirichlet', 0.0]
-    BC_right = ['Neumann', -1.0]
+    BC_right = ['Neumann', 1.0]
     BC_bottom = ['Neumann', 0.0]
     BC_top = ['Neumann', 0.0]
 
-    node_type = ['lattice', 10]  #節点の生成方法。lattice,random
-    #node_type = ['random', 100]  #節点の生成方法。lattice,random
+    node_type = ['lattice', 5]  #節点の生成方法。lattice,random
+    #node_type = ['random', 10]  #節点の生成方法。lattice,random
     matrix_type = 'sparse'  #全体行列の形式。basic,sparse
 
     #節点データ生成。Global節点座標、三角形要素の節点番号、境界線分要素の節点番号
@@ -331,7 +332,7 @@ if __name__ == '__main__':
     nod_pos_tri, nod_pos_seg = make_mesh_data()
 
     #メッシュを表示(ポストプロセス)。番号などの有無(True,False)、グラフの表示方法(show,save)
-    visualize_mesh(show_mesh_text=False, mesh_out_type='show')  #メッシュを表示
+    visualize_mesh(nod_pos_glo, show_text=True, out_type='show')
 
 
     ##### メインプロセス #####
@@ -346,10 +347,10 @@ if __name__ == '__main__':
     mat_A_glo, vec_b_glo = assemble_global_matrix(matrix_type)
 
     #境界要素の情報を設定
-    leng_seg, BC_type = make_boundary_info(nod_pos_seg)
+    BC_type, leng_seg = make_boundary_info(nod_pos_seg)
 
     #境界条件を実装
-    mat_A_glo, vec_b_glo = set_boundary_condition(mat_A_glo, vec_b_glo, BC_type)
+    mat_A_glo, vec_b_glo = set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, leng_seg)
 
     #連立方程式を解く
     unknown_vec_u = solve_simultaneous_equations(mat_A_glo, vec_b_glo)
@@ -359,4 +360,4 @@ if __name__ == '__main__':
     print ("Calculation time: {:0.5f}[sec]".format(compute_time))
 
     #計算結果を表示(ポストプロセス)。番号などの有無(True,False)、グラフの表示方法(show,save)
-    visualize_result(show_result_text=False, result_out_type='show')  #計算結果を表示
+    visualize_result(nod_pos_glo, unknown_vec_u, show_text=False, out_type='show')
