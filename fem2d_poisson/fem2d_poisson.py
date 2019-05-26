f03260b381dc16d104198a9a2aa1781d8212ede9
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-05-27 03:06:35 +0900

    Update fem2d_poisson.py
    境界条件の実装部分などを変更

--- fem2d_poisson/fem2d_poisson.py
+++ fem2d_poisson/fem2d_poisson.py
@@ -86,15 +86,15 @@ def make_mesh_data():
 
 
 #要素行列の構築
-def assemble_element_matrix():
-    #各要素の面積を計算
+def assemble_element_matrix(nod_num_tri, nod_pos_tri):
+    #各要素の面積
     print('Element area_tri')
     area_tri = (nod_pos_tri[:,1,0]-nod_pos_tri[:,0,0])*(nod_pos_tri[:,2,1]-nod_pos_tri[:,0,1])  \
               -(nod_pos_tri[:,2,0]-nod_pos_tri[:,0,0])*(nod_pos_tri[:,1,1]-nod_pos_tri[:,0,1])
     area_tri = np.absolute(area_tri)/2.0
     print(area_tri)
 
-    #各要素の形状関数の係数を計算
+    #各要素の形状関数の係数
     print('Shape function a,b,c')
     shape_a = np.empty((len(nod_pos_tri),3), np.float64)
     shape_b = np.empty((len(nod_pos_tri),3), np.float64)
@@ -114,7 +114,18 @@ def assemble_element_matrix():
     for e in range(min(len(nod_pos_tri),10)):  #形状関数の係数を10番目の三角形要素まで確認
         print(shape_a[e,:], shape_b[e,:], shape_c[e,:])
 
-    return area_tri, shape_a, shape_b, shape_c
+    #要素行列の初期化
+    mat_A_ele = np.zeros((len(nod_num_tri),3,3), np.float64)  #要素係数行列(ゼロで初期化)
+    vec_b_ele = np.zeros((len(nod_num_tri),3), np.float64)  #要素係数ベクトル(ゼロで初期化)
+
+    print("Local matrix")
+    for e in range(len(nod_pos_tri)):
+        for i in range(3):
+            for j in range(3):
+                mat_A_ele[e,i,j] = (shape_b[e,i]*shape_b[e,j] +shape_c[e,i]*shape_c[e,j]) / (4.0*area_tri[e])
+            vec_b_ele[e,i] = -func_f *area_tri[e]/3.0
+
+    return mat_A_ele, vec_b_ele, area_tri
 
 
 #全体行列の構築
@@ -132,9 +143,8 @@ def assemble_global_matrix(matrix_type):
     for e in range(len(nod_pos_tri)):
         for i in range(3):
             for j in range(3):
-                mat_A_glo[ nod_num_tri[e,i], nod_num_tri[e,j] ] \
-                    += 1.0/(4*area_tri[e]) *(shape_b[e,i]*shape_b[e,j] +shape_c[e,i]*shape_c[e,j])
-            vec_b_glo[ nod_num_tri[e,i] ] += -func_f *area_tri[e]/3.0
+                mat_A_glo[ nod_num_tri[e,i], nod_num_tri[e,j] ] += mat_A_ele[e,i,j]
+            vec_b_glo[ nod_num_tri[e,i] ] += vec_b_ele[e,i]
 
         #処理の経過を％表示
         if(CountPercent <= 100*e/len(nod_pos_tri)):
@@ -147,6 +157,7 @@ def assemble_global_matrix(matrix_type):
         for j in range(min(len(nod_pos_glo),10)):
             print("{:7.2f}".format(mat_A_glo[i,j]), end='')
         print(";{:7.2f}".format(vec_b_glo[i]))
+    #print(np.concatenate((mat_A_glo, np.reshape(vec_b_glo, (-1,1))), axis=1))
 
     return mat_A_glo, vec_b_glo
 
@@ -156,49 +167,54 @@ def make_boundary_info(nod_pos_seg):
     #境界線分要素の長さ
     leng_seg = (nod_pos_seg[:,0,0]-nod_pos_seg[:,1,0])**2.0 +(nod_pos_seg[:,0,1]-nod_pos_seg[:,1,1])**2.0
     leng_seg = np.sqrt(leng_seg)
-    print(leng_seg)
+    print('leng_seg =\n', leng_seg)
 
     #境界要素の種類を分類
     BC_type = [""]*len(nod_pos_seg)
-    #BC_type = np.empty((len(nod_pos_seg),2))
+    BC_value = [""]*len(nod_pos_seg)
+    BC_threshold = 0.5*np.mean(leng_seg)
     for e in range(len(nod_pos_seg)):
-        if(nod_pos_seg[e,0,0] <= x_min+0.1*leng_seg[e] and
-           nod_pos_seg[e,1,0] <= x_min+0.1*leng_seg[e]):  #左側境界
-            BC_type[e] = BC_left
-            #BC_type[e,:] = BC_left[:]
-        elif(x_max-0.1*leng_seg[e] <= nod_pos_seg[e,0,0] and
-             x_max-0.1*leng_seg[e] <= nod_pos_seg[e,1,0]):  #右側境界
-            BC_type[e] = BC_right
-            #BC_type[e,:] = BC_right[:]
-        elif(nod_pos_seg[e,0,1] <= y_min+0.1*leng_seg[e] and
-             nod_pos_seg[e,1,1] <= y_min+0.1*leng_seg[e]):  #下側境界
-            BC_type[e] = BC_bottom
-            #BC_type[e,:] = BC_bottom[:]
-        elif(y_max-0.1*leng_seg[e] <= nod_pos_seg[e,0,1] and
-             y_max-0.1*leng_seg[e] <= nod_pos_seg[e,1,1]):  #上側境界
-            BC_type[e] = BC_top
-            #BC_type[e,:] = BC_top[:]
+        if(nod_pos_seg[e,0,0] < x_min +BC_threshold and
+           nod_pos_seg[e,1,0] < x_min +BC_threshold):  #左側境界
+            BC_type[e] = BC_left[0]
+            BC_value[e] = BC_left[1]
+        elif(x_max -BC_threshold < nod_pos_seg[e,0,0] and
+             x_max -BC_threshold < nod_pos_seg[e,1,0]):  #右側境界
+            BC_type[e] = BC_right[0]
+            BC_value[e] = BC_right[1]
+        elif(nod_pos_seg[e,0,1] < y_min +BC_threshold and
+             nod_pos_seg[e,1,1] < y_min +BC_threshold):  #下側境界
+            BC_type[e] = BC_bottom[0]
+            BC_value[e] = BC_bottom[1]
+        elif(y_max -BC_threshold < nod_pos_seg[e,0,1] and
+             y_max -BC_threshold < nod_pos_seg[e,1,1]):  #上側境界
+            BC_type[e] = BC_top[0]
+            BC_value[e] = BC_top[1]
+        else:  #それ以外はNeumann境界にしておく（何もしない）
+            BC_type[e] = 'Neumann'
+            BC_value[e] = 0.0
     print('BC_type =\n', BC_type)
+    print('BC_value =\n', BC_value)
 
-    return BC_type, leng_seg
+    return BC_type, BC_value, leng_seg
 
 
 #境界条件を実装
-def set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, leng_seg):
+def set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, BC_value, leng_seg):
     #各要素の各節点に対応したGlobal節点に対して処理する
     print('Boundary conditions')
     CountPercent = 1
     for e in range(len(nod_pos_seg)):
         for n in range(2):
-            if(BC_type[e][0]=='Dirichlet'):
-                vec_b_glo[:] -= BC_type[e][1]*mat_A_glo[nod_num_seg[e,n],:]  #移項
-                vec_b_glo[nod_num_seg[e,n]] = BC_type[e][1]  #関数を任意の値で固定
+            if(BC_type[e]=='Dirichlet'):
+                vec_b_glo[:] -= BC_value[e]*mat_A_glo[nod_num_seg[e,n],:]  #移項
+                vec_b_glo[nod_num_seg[e,n]] = BC_value[e]  #関数を任意の値で固定
                 mat_A_glo[nod_num_seg[e,n],:] = 0.0  #行を全て0にする
                 mat_A_glo[:,nod_num_seg[e,n]] = 0.0  #列を全て0にする
                 mat_A_glo[nod_num_seg[e,n],nod_num_seg[e,n]] = 1.0  #対角成分は1にする
 
-            if (BC_type[e][0]=='Neumann'):  #Neumann境界条件の処理
-                vec_b_glo[nod_num_seg[e,n]] += BC_type[e][1]*leng_seg[e]/2.0  #関数を任意の傾きで固定
+            if (BC_type[e]=='Neumann'):  #Neumann境界条件の処理
+                vec_b_glo[nod_num_seg[e,n]] += BC_value[e]*leng_seg[e]/2.0  #関数を任意の傾きで固定
 
         #処理の経過を％表示
         if(CountPercent <= 100*e/len(nod_pos_seg)):
@@ -206,12 +222,12 @@ def set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, leng_seg):
             CountPercent += 1
     print(" 100.00%")
 
-    #全体行列を10行10列まで確認
-    print('Post global matrix')
-    for i in range(min(len(nod_pos_glo),10)):
+    print("Post global matrix")
+    for i in range(min(len(nod_pos_glo),10)):   #全体行列を10行10列まで確認
         for j in range(min(len(nod_pos_glo),10)):
             print("{:7.2f}".format(mat_A_glo[i,j]), end='')
         print(";{:7.2f}".format(vec_b_glo[i]))
+    #print(np.concatenate((mat_A_glo, np.reshape(vec_b_glo, (-1,1))), axis=1))
 
     return mat_A_glo, vec_b_glo
 
@@ -238,7 +254,7 @@ def solve_simultaneous_equations(mat_A_glo, vec_b_glo):
 
 
 #メッシュを表示
-def visualize_mesh(show_text, out_type):
+def visualize_mesh(nod_pos_glo, show_text, out_type):
     #plt.rcParams['font.family'] = 'Times New Roman' #全体のフォントを設定
     fig = plt.figure(figsize=(8, 6), dpi=100, facecolor='#ffffff')  #図の設定
 
@@ -321,8 +337,9 @@ if __name__ == '__main__':
     BC_bottom = ['Neumann', 0.0]
     BC_top = ['Neumann', 0.0]
 
-    node_type = ['lattice', 5]  #節点の生成方法。lattice,random
-    #node_type = ['random', 10]  #節点の生成方法。lattice,random
+    #節点の生成方法。lattice,random
+    node_type = ['lattice', 10]  #数字は格子分割におけるx・y方向の節点数
+    #node_type = ['random', 50]  #数字はランダム分割における節点数
     matrix_type = 'sparse'  #全体行列の形式。basic,sparse
 
     #節点データ生成。Global節点座標、三角形要素の節点番号、境界線分要素の節点番号
@@ -341,16 +358,16 @@ if __name__ == '__main__':
     compute_time = time.time()  #計算の開始時刻
 
     #要素行列の構築
-    area_tri, shape_a, shape_b, shape_c = assemble_element_matrix()
+    mat_A_ele, vec_b_ele, area_tri = assemble_element_matrix(nod_num_tri, nod_pos_tri)
 
     #全体行列の構築
     mat_A_glo, vec_b_glo = assemble_global_matrix(matrix_type)
 
     #境界要素の情報を設定
-    BC_type, leng_seg = make_boundary_info(nod_pos_seg)
+    BC_type, BC_value, leng_seg = make_boundary_info(nod_pos_seg)
 
     #境界条件を実装
-    mat_A_glo, vec_b_glo = set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, leng_seg)
+    mat_A_glo, vec_b_glo = set_boundary_condition(mat_A_glo, vec_b_glo, BC_type, BC_value, leng_seg)
 
     #連立方程式を解く
     unknown_vec_u = solve_simultaneous_equations(mat_A_glo, vec_b_glo)
